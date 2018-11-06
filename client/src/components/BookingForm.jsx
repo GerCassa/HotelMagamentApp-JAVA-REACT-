97cb4cca0136200e4cb2590b38a5b3d14b4d0a98
GerCassa <gercassa@gmail.com>
2018-11-06 13:46:03 +0000

    created bookingForm file

---
+++ client/src/components/BookingForm.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class BookingForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: '',
+      length: '',
+      guests: '',
+      firstName: '',
+      lastName: ''
+    }
+  }
+
+  render() {
+    return()
+  }
+}
+
+export default BookingForm;
