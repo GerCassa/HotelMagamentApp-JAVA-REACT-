8b221968287417131a39e79ff1c64b9c1454c348
GerCassa <gercassa@gmail.com>
2018-11-06 22:57:46 +0000

    booking form refactored

--- client/src/components/Bookings/BookingForm.jsx
+++ client/src/components/Bookings/BookingForm.jsx
@@ -1,87 +1,87 @@
 import React from 'react';
 
-class BookingForm extends React.Component {
+const BookingForm = (props) => {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: '',
-      length: '',
-      people: '',
-      firstName: '',
-      lastName: ''
-    }
-
-    this.handleDateChange = this.handleDateChange.bind(this);
-    this.handleLengthChange = this.handleLengthChange.bind(this);
-    this.handleGuestsChange = this.handleGuestsChange.bind(this);
-    this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
-    this.handleLastNameChange = this.handleLastNameChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleDateChange(event) {
-    this.setState({ date: event.target.value })
-  }
-
-  handleLengthChange(event) {
-    this.setState({ length: event.target.value })
-  }
-
-  handleGuestsChange(event) {
-    this.setState({ people: event.target.value })
-  }
-
-  handleFirstNameChange(event) {
-    this.setState({ firstName: event.target.value })
-  }
-
-  handleLastNameChange(event) {
-    this.setState({ lastName: event.target.value })
-  }
-
-  handleSubmit(event) {
+  function handleSubmit(event) {
     event.preventDefault();
-    console.log('form submitted');
-    console.log(this)
-    const date = this.state.date;
-    const length = this.state.length;
-    const people = this.state.people;
-    const firstName = this.state.firstName;
-    const lastName = this.state.lastName;
-    if (!date || !length || !people || !firstName || !lastName) {
-      return;
+    const booking = {
+      "date": event.target.date.value,
+      "length": event.target.length.value,
+      "people": event.target.people.value,
+      "firstName": event.target.firstName.value,
+      "lastName": event.target.lastName.value
     }
-    const newBooking = { date: date, length: length, people: people, firstName: firstName, lastName: lastName };
-
-    this.props.onBookingSubmit(newBooking)
-
-    this.setState({ date: '', length: '', people: '', firstName: '', lastName: '' })
+    props.handleBookingPost(booking);
   }
 
+  return(
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Date" name="date" />
 
-  render() {
-    return(
-      <form onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="Date"
-          value={this.state.date} onChange={this.handleDateChange} />
+      <input type="number" placeholder="How many nights" name="length" />
 
-        <input type="number" placeholder="How many nights"
-          value={this.state.length} onChange={this.handleLengthChange} />
+      <input type="number" placeholder="How many guest" name="people" />
 
-        <input type="number" placeholder="How many guest"
-          value={this.state.people} onChange={this.handleGuestsChange} />
+      <input type="text" placeholder="First Name" name="firstName" />
 
-        <input type="text" placeholder="First Name"
-          value={this.state.firstName} onChange={this.handleFirstNameChange} />
+      <input type="text" placeholder="Last Name" name="lastName" />
 
-        <input type="text" placeholder="Last Name"
-          value={this.state.lastName} onChange={this.handleLastNameChange} />
-
-        <input type="submit" value="Save Booking" />
-      </form>
+      <input type="submit" value="Save Booking" />
+    </form>
     )
-  }
 }
 
 export default BookingForm;
+
+
+// OLD VERSION
+
+  //   this.handleDateChange = this.handleDateChange.bind(this);
+  //   this.handleLengthChange = this.handleLengthChange.bind(this);
+  //   this.handleGuestsChange = this.handleGuestsChange.bind(this);
+  //   this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
+  //   this.handleLastNameChange = this.handleLastNameChange.bind(this);
+  //   this.handleSubmit = this.handleSubmit.bind(this);
+  // }
+
+  // handleDateChange(event) {
+  //   this.setState({ date: event.target.value })
+  // }
+
+  // handleLengthChange(event) {
+  //   this.setState({ length: event.target.value })
+  // }
+
+  // handleGuestsChange(event) {
+  //   this.setState({ people: event.target.value })
+  // }
+
+  // handleFirstNameChange(event) {
+  //   this.setState({ firstName: event.target.value })
+  // }
+
+  // handleLastNameChange(event) {
+  //   this.setState({ lastName: event.target.value })
+  // }
+
+  // handleSubmit(event) {
+  //   event.preventDefault();
+  //   console.log('form submitted');
+  //   console.log(this)
+  //   const date = this.state.date;
+  //   const length = this.state.length;
+  //   const people = this.state.people;
+  //   const firstName = this.state.firstName;
+  //   const lastName = this.state.lastName;
+  //   if (!date || !length || !people || !firstName || !lastName) {
+  //     return;
+  //   }
+  //   const newBooking = { date: date, length: length, people: people, firstName: firstName, lastName: lastName };
+
+  //   this.props.onBookingSubmit(newBooking)
+
+  //   this.setState({ date: '', length: '', people: '', firstName: '', lastName: '' })
+  // }
+
+
+  // render() {
